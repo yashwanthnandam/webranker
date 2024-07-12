1fa2c4ba6c6b522b589731f217ec10b7e36bfa25
Yashwanth <yashwanth@Yashwanths-MacBook-Air.local>
2024-07-12 09:49:39 +0530

    test

--- urlranker/app.py
+++ urlranker/app.py
@@ -6,6 +6,11 @@ import os
 def create_app():
     load_dotenv()
     api_key = os.getenv("OPENAI_API_KEY")
+    print("printing api key ---------------------------->")
+    print(api_key)
+    if not api_key:
+        print("Error: OPENAI_API_KEY not found in the environment variables.")
+        return None 
     model_name = "gpt-3.5-turbo"
     gpt_service = GPTService(api_key,model_name)
     controller = HTMLProcessingController(gpt_service)
